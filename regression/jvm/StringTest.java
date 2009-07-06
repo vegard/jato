d9605aed91c2cfba36b6b24295555de9d73876b2
Vegard Nossum <vegard.nossum@gmail.com>
2009-07-06 20:07:54 +0200

    regression: retab StringTest and encode unicode with \uXXXX syntax
    Signed-off-by: Vegard Nossum <vegard.nossum@gmail.com>

--- regression/jvm/StringTest.java
+++ regression/jvm/StringTest.java
@@ -4,25 +4,25 @@ package jvm;
  * @author Vegard Nossum <vegard.nossum@gmail.com>
  */
 public class StringTest extends TestCase {
-	public static void testUnicode() {
-		String s = "æøå";
-		assertEquals(s.length(), 3);
+    public static void testUnicode() {
+        String s = "\u00c6\u00d8\u00e5";
+        assertEquals(s.length(), 3);
 
-		String t = "ヒラガナ";
-		assertEquals(t.length(), 4);
-	}
+        String t = "\u306a\u308b\u3068";
+        assertEquals(t.length(), 3);
+    }
 
-	public static void testStringConcatenation() {
-		String a = "123";
-		String b = "abcd";
+    public static void testStringConcatenation() {
+        String a = "123";
+        String b = "abcd";
 
-		assertObjectEquals("123abcd", a + b);
-	}
+        assertObjectEquals("123abcd", a + b);
+    }
 
-	public static void main(String args[]) {
-		testUnicode();
-		testStringConcatenation();
+    public static void main(String args[]) {
+        testUnicode();
+        testStringConcatenation();
 
-		exit();
-	}
+        exit();
+    }
 }
